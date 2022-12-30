e222fb8880287b192d25971ca9018001592a8c45
Tobias Scholze <tobias.scholze+git@me.com>
2022-12-30 19:28:15 +0100

    Added the feature to add social media icons to the root container template using the configuration.
    re #38

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,29 @@
 import io.github.tscholze.kotlog.Kotlog
+import io.github.tscholze.kotlog.models.BlogConfiguration
+import io.github.tscholze.kotlog.models.SocialMedia
+import io.github.tscholze.kotlog.models.SocialMediaPlatform
 
 fun main(args: Array<String>) {
 
     // 1. Create a configuration for the blog,
-    // or it will be fetched from `~/.kotlog file`
-    //
-    // Sample:
 
-    // val configuration = BlogConfiguration(
-    //     baseUrl = "https://tscholze.github.io/blog",
-    //     titleText = "Tobias Scholze | The Stuttering Nerd",
-    //     footerText = "Made with ❤️ without JavaScript| Kotlog | Tobias Scholze",
-    //     outputDirectoryName = "www"
-    //)
+    // 1.1
+    // fetched from `~/.kotlog file`
+    //
+    // 1.2.
+    // Use a configuration object in code.
+    val configuration = BlogConfiguration(
+        baseUrl = "https://tscholze.github.io/blog",
+        titleText = "Tobias Scholze | The Stuttering Nerd",
+        footerText = "Made with ❤️ without JavaScript| Kotlog | Tobias Scholze",
+        outputDirectoryName = "www",
+        socialMedia = listOf(
+            SocialMedia(SocialMediaPlatform.GITHUB, "tscholze"),
+            SocialMedia(SocialMediaPlatform.TWITTER, "tobonautilus"),
+            SocialMedia(SocialMediaPlatform.MASTODON, "@tobonaut@mastodon.social", "https://mastodon.social/@tobonaut")
+        )
+    )
 
     // 2. Call and run Kotlog with command line arguments and configuration.
-    Kotlog(args)
+    Kotlog(args, configuration)
 }
-
